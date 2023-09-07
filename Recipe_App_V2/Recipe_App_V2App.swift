3389d5237b5f38ff08df6e21c8b8040b5bf6fe1e
tayamirai9 <128257364+tayamirai9@users.noreply.github.com>
2023-09-07 10:00:51 +0100

    Initial Commit

---
+++ Recipe_App_V2/Recipe_App_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_App_V2App.swift
+//  Recipe_App_V2
+//
+//  Created by Tayami Rai on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_App_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe_App_V2/Views/RecipeFeaturedView.swift
@@ -0,0 +1,97 @@
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    @State var isDetailViewShowing = false
+    @State var tabSelectionIndex = 0
+    
+    var body: some View {
+        
+        let featuredRecipes = model.recipes.filter({ $0.featured })
+        
+        VStack(alignment: .leading, spacing: 0) {
+        
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(Font.custom("Avenir Heavy", size: 24))
+                
+            
+            GeometryReader { geo in
+            
+            TabView (selection: $tabSelectionIndex) {
+                
+                // Loop through each recipe
+                ForEach (0..<featuredRecipes.count) { index in
+                   
+                    // Recipe card button
+                    Button(action: {
+                        
+                        // Show the recipe detail sheet
+                        self.isDetailViewShowing = true
+                        
+                    }, label: {
+                        
+                        // Recipe card
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                                
+                            
+                            VStack(spacing: 0) {
+                                Image(featuredRecipes[index].image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipped()
+                                Text(featuredRecipes[index].name)
+                                    .padding(5)
+                                    .font(Font.custom("Avenir", size: 15))
+                            }
+                        }
+                        
+                    })
+                    .tag(index)
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(15)
+                    .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                    
+                    
+                }
+                
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+        }
+        
+            VStack (alignment: .leading, spacing: 10) {
+                
+                Text("Preparation Time:")
+                    .font(Font.custom("Avenir Heavy", size: 16))
+                Text(model.recipes[tabSelectionIndex].prepTime)
+                    .font(Font.custom("Avenir", size: 15))
+                
+                Text("Highlights")
+                    .font(Font.custom("Avenir Heavy", size: 16))
+                RecipeHighlights(highlights: model.recipes[tabSelectionIndex].highlights)
+            }
+            .padding([.leading, .bottom])
+        }
+        .sheet(isPresented: $isDetailViewShowing) {
+            // Show the Recipe Detail View
+            RecipeDetailView(recipe: featuredRecipes[tabSelectionIndex])
+        }
+    }
+    
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+            .environmentObject(RecipeModel())
+    }
+}
+
